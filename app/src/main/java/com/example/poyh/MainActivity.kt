e45791abf47a8db46172b44ac102cfbab88e1baf
Keing57 <92033975+Keing57@users.noreply.github.com>
2026-09-18 12:02:05 +0200

    Create main menu UI layout with Play button

--- app/src/main/java/com/example/poyh/MainActivity.kt
+++ app/src/main/java/com/example/poyh/MainActivity.kt
@@ -1,11 +1,24 @@
 package com.example.poyh
 
 import android.os.Bundle
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val btnPlay = findViewById<MaterialButton>(R.id.btnPlay)
+        val btnSettings = findViewById<MaterialButton>(R.id.btnSettings)
+
+        btnPlay.setOnClickListener {
+            Toast.makeText(this, "Starting Game...", Toast.LENGTH_SHORT).show()
+        }
+
+        btnSettings.setOnClickListener {
+            Toast.makeText(this, "Opening Settings...", Toast.LENGTH_SHORT).show()
+        }
     }
 }
